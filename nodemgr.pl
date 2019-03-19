2fc6a4c5b9d2cd12fd02234cf959b37a118472d4
rancedelong <rancedelong@users.noreply.github.com>
2019-03-19 19:09:15 +0100

    Add files via upload

---
+++ nodemgr.pl
@@ -0,0 +1,10 @@
+% Next Generation Access Control node manager
+% stub for unified node manager to be developed
+
+:- module(nodemgr, [nodemgr/3]).
+:- use_module([ pep, rap ]).
+
+
+
+nodemgr(PEPport,PDPport,RAPport) :-
+	true.
